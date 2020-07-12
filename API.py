da923bdfed520793268c9a3cd0451c84bc50dae2
Schoss <33396314+Schoss@users.noreply.github.com>
2020-07-12 09:53:07 +0200

    Proposal

--- API.py
+++ API.py
@@ -23,3 +23,27 @@ for symbol in symbols:
 
 SomeCommand > SomeFile.txt  
 
+import alpha_vantage
+from alpha_vantage import *
+import json
+import requests
+import csv, xls
+
+API_URL = "https://www.alphavantage.co/query" 
+symbols = ['QCOM',"INTC","PDD","AAPL"]
+
+for symbol in symbols:
+        data = { "function": "TIME_SERIES_INTRADAY", 
+        "symbol": symbol,
+        "interval" : "60min",       
+        "datatype": "json", 
+        "apikey": "xx" } 
+        response = requests.get(API_URL, data) 
+        data = response.json()
+        print(symbol)
+        a = (data['Time Series (120min)'])
+        keys = (a.keys())
+        for key in keys:
+                print(a[key]['10. high'] + " " + a[key]['5. volume'])
+
+SomeCommand > SomeFile.txt  
